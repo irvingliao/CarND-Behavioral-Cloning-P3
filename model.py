b7e367ec1971763ca5fab8e04d70bd1a911460fa
Kenny Liao <irvingliao@gmail.com>
2019-01-31 22:21:35 -0800

    Model 01: Epoch: 50 loss: 0.0015 - val_loss: 0.0368

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ import random
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
-dataPath = './training_data/03/'
+dataPath = './training_data/01/'
 
 #load csv file
 def loadDrivingLog(path):
@@ -17,6 +17,31 @@ def loadDrivingLog(path):
         for line in reader:
             lines.append(line)
     return lines
+
+import sys
+# Print iterations progress
+def print_progress(iteration, total):
+    """
+    Call in a loop to create terminal progress bar
+    
+    Parameters
+    ----------
+        
+    iteration : 
+                Current iteration (Int)
+    total     : 
+                Total iterations (Int)
+    """
+    str_format = "{0:.0f}"
+    percents = str_format.format(100 * (iteration / float(total)))
+    filled_length = int(round(100 * iteration / float(total)))
+    bar = '█' * filled_length + '-' * (100 - filled_length)
+
+    sys.stdout.write('\r |%s| %s%%' % (bar, percents)),
+
+    if iteration == total:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
 
 #generator to yeild processed images for training as well as validation data set
 def img_generator(data, batchSize = 32):
@@ -54,7 +79,8 @@ def img_generator(data, batchSize = 32):
 def loadImages(lines):
     X = []
     y = []
-    for line in lines:
+    for i in range(len(lines)):
+        line = lines[i]
         image = cv2.imread(dataPath + 'IMG/'+ line[0].split('/')[-1])
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         steering_angle = float(line[3])
@@ -68,12 +94,13 @@ def loadImages(lines):
         l_img = cv2.imread(dataPath + 'IMG/'+ line[1].split('/')[-1])
         l_img = cv2.cvtColor(l_img, cv2.COLOR_BGR2RGB)
         X.append(l_img)
-        y.append(steering_angle+0.4)
+        y.append(steering_angle+0.22)
         # appending right camera image and steering angle with offset
         r_img = cv2.imread(dataPath + 'IMG/'+ line[2].split('/')[-1])
         r_img = cv2.cvtColor(r_img, cv2.COLOR_BGR2RGB)
         X.append(r_img)
-        y.append(steering_angle-0.3)
+        y.append(steering_angle-0.22)
+        print_progress(i+1, len(lines))
 
     return np.array(X), np.array(y)
 
@@ -83,30 +110,36 @@ X_train, y_train = loadImages(lines)
 
 from keras.models import Sequential
 from keras.layers import Convolution2D, Dropout, MaxPooling2D, Flatten, Activation, Dense, Cropping2D, Lambda
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 
 #creating model to be trained
 model = Sequential()
 model.add(Lambda(lambda x: x /255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,20),(0,0))))
-model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='elu'))
 # model.add(Dropout(0.75))
 # model.add(MaxPooling2D())
-model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='relu'))
-model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation='relu'))
-model.add(Convolution2D(64, 3, 3, activation='relu'))
-model.add(Convolution2D(64, 3, 3, activation='relu'))
-# model.add(Dropout(0.5))
+model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='elu'))
+model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation='elu'))
+model.add(Convolution2D(64, 3, 3, activation='elu'))
+model.add(Convolution2D(64, 3, 3, activation='elu'))
+model.add(Dropout(0.5))
 # model.add(MaxPooling2D())
 model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
+model.add(Dense(100),activation='elu')
+model.add(Dense(50),activation='elu')
+model.add(Dense(10),activation='elu')
 model.add(Dense(1))
 
 #compiling and running the model
 model.compile(optimizer='adam', loss='mse')
 # history_object = model.fit_generator(img_generator(training), samples_per_epoch=len(training)*4, nb_epoch = 2, validation_data=img_generator(valid), nb_val_samples=len(valid), verbose=1)
-history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, batch_size=32, epochs=10, verbose=1)
+
+callbacks = [
+    EarlyStopping(patience=5, monitor='loss', min_delta=0, mode='min'),
+    ModelCheckpoint('model_best.h5', monitor='loss', save_best_only=True, verbose=1)
+]
+history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, batch_size=32, epochs=50, verbose=1, callbacks=callbacks)
 
 print(history_object.history.keys())
 
