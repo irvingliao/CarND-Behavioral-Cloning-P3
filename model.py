ce333e5a235c97c3d185c856820bc0bb501eb592
Kenny Liao <irvingliao@gmail.com>
2019-02-03 11:04:26 -0800

    Remove flipping augmentation.

--- model.py
+++ model.py
@@ -59,8 +59,8 @@ def img_generator(data, batchSize = 32):
                 X_batch.append(image)
                 y_batch.append(steering_angle)
                 #appending flipped image
-                X_batch.append(np.fliplr(image))
-                y_batch.append(-steering_angle)
+                # X_batch.append(np.fliplr(image))
+                # y_batch.append(-steering_angle)
                 # appending left camera image and steering angle with offset
                 l_img = cv2.imread(dataPath + 'IMG/'+ line[1].split('/')[-1])
                 l_img = cv2.cvtColor(l_img, cv2.COLOR_BGR2RGB)
@@ -70,7 +70,7 @@ def img_generator(data, batchSize = 32):
                 r_img = cv2.imread(dataPath + 'IMG/'+ line[2].split('/')[-1])
                 r_img = cv2.cvtColor(r_img, cv2.COLOR_BGR2RGB)
                 X_batch.append(r_img)
-                y_batch.append(steering_angle-0.3)
+                y_batch.append(steering_angle-0.4)
             # converting to numpy array
             X_batch = np.array(X_batch)
             y_batch = np.array(y_batch)
@@ -88,23 +88,27 @@ def loadImages(lines):
         X.append(image)
         y.append(steering_angle)
         #appending flipped image
-        X.append(np.fliplr(image))
-        y.append(-steering_angle)
+        # X.append(np.fliplr(image))
+        # y.append(-steering_angle)
         # appending left camera image and steering angle with offset
         l_img = cv2.imread(dataPath + 'IMG/'+ line[1].split('/')[-1])
         l_img = cv2.cvtColor(l_img, cv2.COLOR_BGR2RGB)
         X.append(l_img)
-        y.append(steering_angle+0.25)
+        y.append(steering_angle+0.4)
         # appending right camera image and steering angle with offset
         r_img = cv2.imread(dataPath + 'IMG/'+ line[2].split('/')[-1])
         r_img = cv2.cvtColor(r_img, cv2.COLOR_BGR2RGB)
         X.append(r_img)
-        y.append(steering_angle-0.25)
+        y.append(steering_angle-0.4)
         print_progress(i+1, len(lines))
 
     return np.array(X), np.array(y)
 
 lines = loadDrivingLog(dataPath)
+# line = lines[0]
+# print('line: ', line)
+# path = dataPath + 'IMG/'+ line[0].split('\\')[-1]
+# print('path: ', path)
 # training, valid = train_test_split(lines, test_size = 0.2)
 X_train, y_train = loadImages(lines)
 
@@ -117,29 +121,26 @@ model = Sequential()
 model.add(Lambda(lambda x: x /255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,20),(0,0))))
 model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='elu'))
-# model.add(Dropout(0.75))
-# model.add(MaxPooling2D())
 model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='elu'))
 model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation='elu'))
 model.add(Convolution2D(64, 3, 3, activation='elu'))
 model.add(Convolution2D(64, 3, 3, activation='elu'))
-# model.add(Dropout(0.5))
-# model.add(MaxPooling2D())
+model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100, activation='elu'))
-model.add(Dense(50, activation='elu'))
-model.add(Dense(10, activation='elu'))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 #compiling and running the model
 model.compile(optimizer='adam', loss='mse')
-# history_object = model.fit_generator(img_generator(training), samples_per_epoch=len(training)*4, nb_epoch = 2, validation_data=img_generator(valid), nb_val_samples=len(valid), verbose=1)
 
 callbacks = [
     EarlyStopping(patience=10, monitor='loss', min_delta=0, mode='min'),
     ModelCheckpoint('model_best.h5', monitor='loss', save_best_only=True, verbose=1)
 ]
-history_object = model.fit(X_train, y_train, validation_split=0.1, shuffle=True, batch_size=32, epochs=60, verbose=1, callbacks=callbacks)
+history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, batch_size=32, epochs=60, verbose=1, callbacks=callbacks)
+# history_object = model.fit_generator(img_generator(training), samples_per_epoch=len(training)*4, nb_epoch = 15, validation_data=img_generator(valid), nb_val_samples=len(valid), verbose=1, callbacks=callbacks)
 
 # print(history_object.history.keys())
 
